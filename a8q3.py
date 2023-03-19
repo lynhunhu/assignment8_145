c90f687d3e9a59746f3893e6371d70ecf1348fee
lynhunhu <lynhunhu2.3@gmail.com>
2023-03-19 02:56:22 -0600

    finished a8q3 copy(node_chain) second ver

--- a8q3.py
+++ a8q3.py
@@ -1,11 +1,33 @@
+#Name: Nhu Nhu Ly
+#NSID: cvd326
+#instructor's name: Eric Neufeld
+#student number: 11333935
+#course number: 27177
+#lab section: Monday 1:30 - 2:50 - 28623
 
-def copy(node_chain=N.node):
+import node as N
+
+
+def to_string(node_chain):
+
+    if node_chain is None:
+        return None
+    
+    elif node_chain.get_next() is None:
+        return '[ {} | *-]'.format(node_chain.get_data())
+    
+    else:
+        return '[ {} | *-]-->'.format(node_chain.get_data()) + to_string(node_chain.get_next())
+    
+
+
+def copy(node_chain):
     
     if node_chain is None:
         return None
     
     else:
-        #new_node = N.node(None)
+        new_node = N.node(None)
 
         walker = node_chain
         new_node.set_data(walker.get_data())
@@ -14,7 +36,33 @@ def copy(node_chain=N.node):
 
         return new_node.set_next(copy(walker))
     
+# def replace(node_chain, target, replacement):
+#     walker = node_chain
 
+#     if node_chain is None:
+#         return None
+    
+#     if walker.get_data() == target:
+#         return node_chain(replacement, replace(walker.get_next(), target, replacement))
+    
+#     return node_chain(walker.get_data(), replace(walker.get_next(),target,replacement))
+
+#test
+## vòng lặp
+## 1
+## newnode(1,copy(...))
+#copy(N.node(2))
+
+
+chain= N.node(1,N.node(2))
+
+result = copy(chain)
+expected = N.node(1,N.node(2))
+
+if result == expected:
+    print(True)
+
+    
     
 
 
