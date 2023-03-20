87adf1751d2a284c7778fc2f6034c9a8d6535c73
lynhunhu <lynhunhu2.3@gmail.com>
2023-03-20 13:36:35 -0600

    finished a8q3 all functions

--- a8q3.py
+++ a8q3.py
@@ -25,44 +25,18 @@ def copy(node_chain):
     
     if node_chain is None:
         return None
-    
     else:
-        new_node = N.node(None)
+        return N.node(node_chain.get_data(), copy(node_chain.get_next()))
+      
+        
+def replace(node_chain, target, replacement):
 
-        walker = node_chain
-        new_node.set_data(walker.get_data())
-
-        walker.set_data(walker.get_next())
-
-        return new_node.set_next(copy(walker))
+    if node_chain is None:
+        return None   
     
-# def replace(node_chain, target, replacement):
-#     walker = node_chain
-
-#     if node_chain is None:
-#         return None
+    if node_chain.get_data() == target:
+        return N.node(replacement, replace(node_chain.get_next(), target, replacement))
     
-#     if walker.get_data() == target:
-#         return node_chain(replacement, replace(walker.get_next(), target, replacement))
-    
-#     return node_chain(walker.get_data(), replace(walker.get_next(),target,replacement))
-
-#test
-## vòng lặp
-## 1
-## newnode(1,copy(...))
-#copy(N.node(2))
-
-
-chain= N.node(1,N.node(2))
-
-result = copy(chain)
-expected = N.node(1,N.node(2))
-
-if result == expected:
-    print(True)
-
-    
-    
+    return  N.node(node_chain.get_data(), replace(node_chain.get_next(),target,replacement))
 
 
